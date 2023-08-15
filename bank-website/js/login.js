73bcf3fced912e5d0a6172e09464d04c7174adb6
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-08-15 22:25:09 +0600

    login function complete

--- bank-website/js/login.js
+++ bank-website/js/login.js
@@ -2,3 +2,15 @@
 const btnSubmit = document.getElementById("btn-submit");
 const emailField = document.getElementById("email");
 const passwordField = document.getElementById("password");
+
+// login function
+btnSubmit.addEventListener("click", function (event) {
+  event.preventDefault();
+  const email = emailField.value;
+  const password = passwordField.value;
+  if (email === "nazmul@gmail.com" && password === "1234") {
+    window.location.href = "bank.html";
+  } else {
+    alert("Invalid User! Give your correct information!");
+  }
+});
