db3fe288925b9ba18b3e4d83e5c5802abf274aff
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-08-15 22:20:57 +0600

    hold html element

--- bank-website/js/login.js
+++ bank-website/js/login.js
@@ -1,1 +1,4 @@
-console.log("login file");
+// hold html element
+const btnSubmit = document.getElementById("btn-submit");
+const emailField = document.getElementById("email");
+const passwordField = document.getElementById("password");
