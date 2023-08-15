8d46227f0fe0c8901d163c2c7c1ff6dbaf299a18
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-08-15 22:39:42 +0600

    hold html element on bank.js file

---
+++ bank-website/js/bank.js
@@ -0,0 +1,9 @@
+// hold html element
+const deposit = document.getElementById("deposit");
+const withdraw = document.getElementById("withdraw");
+const balance = document.getElementById("balance");
+
+const depositInput = document.getElementById("depositInput");
+const withdrawInput = document.getElementById("withdrawInput");
+const depositBtn = document.getElementById("depositBtn");
+const withdrawBtn = document.getElementById("withdrawBtn");
