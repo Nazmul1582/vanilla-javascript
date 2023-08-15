c25c43b1b3cfe8d5fe9f77b3710f29a5af35822f
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-08-15 22:43:48 +0600

    deposit function added

--- bank-website/js/bank.js
+++ bank-website/js/bank.js
@@ -7,3 +7,13 @@ const depositInput = document.getElementById("depositInput");
 const withdrawInput = document.getElementById("withdrawInput");
 const depositBtn = document.getElementById("depositBtn");
 const withdrawBtn = document.getElementById("withdrawBtn");
+
+// deposit function
+depositBtn.addEventListener("click", function () {
+  let totalDeposit = parseInt(deposit.innerText);
+  let totalBalance = parseInt(balance.innerText);
+  let depositMoney = parseInt(depositInput.value);
+  deposit.innerText = depositMoney + totalDeposit;
+  balance.innerText = depositMoney + totalBalance;
+  depositInput.value = "";
+});
