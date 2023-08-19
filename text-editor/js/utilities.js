35badbef4c9506eed28d7abb8d9fd95b50d87fb0
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-08-19 18:24:43 +0600

    changing text format completed

--- text-editor/js/utilities.js
+++ text-editor/js/utilities.js
@@ -2,11 +2,11 @@ const textField = document.getElementById("text-area");
 
 // Reusable function for changing text format
 function changeStyle(targetedElement, styledClass) {
-  if (textField.getAttribute("class").includes(styledClass)) {
+  if (textField.className.includes(styledClass)) {
     textField.classList.remove(styledClass);
     targetedElement.classList.remove("focused");
-    return;
+  } else {
+    textField.classList.add(styledClass);
+    targetedElement.classList.add("focused");
   }
-  textField.classList.add(styledClass);
-  targetedElement.classList.add("focused");
 }
