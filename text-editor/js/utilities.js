f60949d58f805f143cdb09733259c2d06953ca66
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-08-19 18:19:36 +0600

    another way to create the reusable function

--- text-editor/js/utilities.js
+++ text-editor/js/utilities.js
@@ -2,7 +2,7 @@ const textField = document.getElementById("text-area");
 
 // Reusable function for changing text format
 function changeStyle(targetedElement, styledClass) {
-  if (textField.className.includes(styledClass)) {
+  if (textField.getAttribute("class").includes(styledClass)) {
     textField.classList.remove(styledClass);
     targetedElement.classList.remove("focused");
     return;
