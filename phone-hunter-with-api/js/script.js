268dc52ca457b330c7b1ce46c41bece17c71119b
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-08-29 16:00:56 +0600

    get data for api

--- phone-hunter-with-api/js/script.js
+++ phone-hunter-with-api/js/script.js
@@ -1,1 +1,8 @@
-console.log("hunting phone")
+const loadPhones = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    const phones = data.data;
+    console.log(phones)
+}
+
+loadPhones();
