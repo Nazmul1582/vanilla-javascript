9767eb42ebdd66034f60d4b617a1c4a7e77a4d24
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-08-30 22:01:01 +0600

    search functionality added

--- phone-hunter-with-api/js/script.js
+++ phone-hunter-with-api/js/script.js
@@ -1,17 +1,17 @@
-const loadPhones = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhones = async (phoneName) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${phoneName}`);
     const data = await res.json();
     const phones = data.data;
-    console.log(phones)
     displayPhones(phones)
 }
 
 const displayPhones = (phones) => {
-    const phoneContainer = document.getElementById('phone-container')
+    const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.textContent = ""
     phones.forEach(phone => {
         const div = document.createElement("div");
         div.innerHTML = `
-        <div class="card bg-white shadow-xl p-5">
+        <div class="card bg-white shadow-2xl p-5">
             <figure class="bg-[#0D6EFD0D] py-10"><img src="${phone.image}" alt="${phone.brand}" /></figure>
             <div class="card-body text-center">
                 <h2 class="card-title text-2xl justify-center mb-3">${phone.phone_name}</h2>
@@ -27,4 +27,15 @@ const displayPhones = (phones) => {
     })
 }
 
-loadPhones();
+// handle search button
+const inputField = document.getElementById("inputField");
+const searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener('click', (e) => handleSearch(e));
+
+const handleSearch = (e) => {
+    e.preventDefault();
+    const phoneName = inputField.value;
+    loadPhones(phoneName)
+    inputField.value = ""
+}
