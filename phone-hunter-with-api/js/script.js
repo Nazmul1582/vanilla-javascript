b6cc71430666bb2886889a9bc6975ba2e3fe334a
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-08-30 10:33:21 +0600

    dynamically show all phones

--- phone-hunter-with-api/js/script.js
+++ phone-hunter-with-api/js/script.js
@@ -3,6 +3,28 @@ const loadPhones = async () => {
     const data = await res.json();
     const phones = data.data;
     console.log(phones)
+    displayPhones(phones)
+}
+
+const displayPhones = (phones) => {
+    const phoneContainer = document.getElementById('phone-container')
+    phones.forEach(phone => {
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <div class="card bg-white shadow-xl p-5">
+            <figure class="bg-[#0D6EFD0D] py-10"><img src="${phone.image}" alt="${phone.brand}" /></figure>
+            <div class="card-body text-center">
+                <h2 class="card-title text-2xl justify-center mb-3">${phone.phone_name}</h2>
+                <p>There are many variations of passages of available, but the majority have suffered</p>
+                <span class="font-bold text-2xl mb-2">$999</span>
+                <div class="card-actions justify-center">
+                <button class="btn text-white capitalize bg-[#0D6EFD] hover:bg-[#0d5efd]">Show Details</button>
+                </div>
+            </div>
+        </div>
+        `
+        phoneContainer.appendChild(div)
+    })
 }
 
 loadPhones();
